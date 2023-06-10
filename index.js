ad198b46a93482e6676b362ff623a7669bf1c09a
Litontripura <litontripura01@gmail.com>
2023-06-10 07:18:06 +0600

    i have created the delet user route

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(express.json());
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
-    return res.status(401).send({error: true, message: " unauthorized access",});
+    return res.status(401).send({error: true, message: "unauthorized access",});
   }
 
-  const token = authorization.split(" ")[1];
+  const token = authorization.split(' ')[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if(err){
@@ -43,14 +43,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const userCollection = client.db("creativeSummer").collection("users");
     const bannerCollection = client.db("creativeSummer").collection("bannerImg");
     const classesCollection = client.db("creativeSummer").collection("classes");
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:"4h"})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:"5h"})
       res.send({token})
     });
     app.post("/users", async (req, res) => {
@@ -67,6 +67,11 @@ async function run() {
       const result = await classesCollection.find().toArray();
       res.send(result);
     });
+    app.post('/classesadd', async(req, res)=>{
+      const body = req.body;
+      const result = await classesCollection.insertOne(body);
+      res.send(result)
+    })
     //  make admin an user
     app.patch('/users/admin/:id', async(req, res)=>{
        const id = req.params.id;
@@ -79,6 +84,9 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc)
       res.send(result)
     })
+    
+
+
     app.get("/banner", async (req, res) => {
       const result = await bannerCollection.find().toArray();
       res.send(result);
@@ -94,6 +102,13 @@ async function run() {
         },
       };
       const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    // Delete a user
+    app.delete('/deletuser/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result= await userCollection.deleteOne(query)
       res.send(result)
     })
     // banner section
